80d65490861c5f435995808722ff98648108610b
Usuario <javichua.a@hotmail.com>
2023-10-19 11:35:58 +0200

    feat: api system from SecondPage and InfoCalendar

--- Components/InfoCalendar.js
+++ Components/InfoCalendar.js
@@ -2,6 +2,12 @@ import React from "react";
 import { View, Text } from "react-native";
 import { sty } from "../Css/StylesInfoCalendar";
 
-export default function InfoCalendar() {
-  return <View style={[sty.infoContainer]}></View>;
+export default function InfoCalendar(props) {
+  let info = props.moon_info;
+
+  return (
+    <View style={[sty.infoContainer]}>
+      <Text style={[sty.whiteText]}>Sunset: {info.sunset}</Text>
+    </View>
+  );
 }
--- Screens/SecondPage.js
+++ Screens/SecondPage.js
@@ -5,15 +5,34 @@ import DatePicker from "react-native-modern-datepicker";
 import { sty } from "../Css/StyleSecondPage";
 import InfoCalendar from "../Components/InfoCalendar";
 import Footer from "../Components/Footer";
+import { API_KEY, API_HOST } from "@env";
 
 export default function SecondPage() {
-  const [date, setDate] = useState(false); //date variable
+  const [date, setDate] = useState(false);
+  const [info, setInfo] = useState("today");
+  const urlAstro = `https://weatherapi-com.p.rapidapi.com/astronomy.json?q=Granada&dt=${date}`; //YYYY-MM-DD
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": API_HOST,
+    },
+  };
 
   function handleChange(propDate) {
     setDate(propDate);
   }
-  function Mensaje(propDate) {
+  async function getCurrentWeather() {
     console.log("FECHA: " + date);
+    try {
+      const res = await fetch(urlAstro, options);
+      const object = await res.json();
+      setInfo(object.astronomy.astro);
+    } catch (error) {}
+  }
+  function mensaje() {
+    console.log("mensaje" + info.sunset);
   }
 
   return (
@@ -35,10 +54,13 @@ export default function SecondPage() {
             mode="calendar"
             onSelectedChange={handleChange}
           />
-          <TouchableOpacity onPress={Mensaje}>
+          <TouchableOpacity onPress={getCurrentWeather}>
             <Text>Boton Fecha</Text>
           </TouchableOpacity>
-          <InfoCalendar />
+          <TouchableOpacity onPress={mensaje}>
+            <Text>confirmar</Text>
+          </TouchableOpacity>
+          <InfoCalendar moon_info={info} />
           <Footer />
         </ScrollView>
       </View>
